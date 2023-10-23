7259b5e55d3c18bbe0f3b96a178448aebb347ecb
spaunder76 <arthurbousquet36@gmail.com>
2023-10-23 17:18:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const category2Container = document.getElementById('Action');
     const category3Container = document.getElementById('Romance');
 
-    // Fonction pour gérer l'affichage des films
     function displayMovies(container, movies, startIndex, count) {
-        // Cacher tous les films
         const allMovies = container.querySelectorAll('.video');
         allMovies.forEach(movie => {
             movie.classList.add('hidden');
         });
 
-        // Afficher les prochains films
         for (let i = startIndex; i < startIndex + count; i++) {
             const movie = movies[i];
             if (movie) {
@@ -25,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Initialiser les compteurs pour chaque catégorie
     let startIndexCategory1 = 0;
     let startIndexCategory2 = 0;
     let startIndexCategory3 = 0;
@@ -33,12 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
 // Fonction pour gérer le bouton "Suivant" pour chaque catégorie
 function handleNextButton(categoryContainer, startIndex) {
     startIndex++;
-    // Vérifier s'il y a plus de films à afficher
     if (startIndex + 3 < categoryContainer.movies.length) {
-        // Cacher le film le plus à gauche
         const leftMostMovie = categoryContainer.querySelector('.video:not(.hidden)');
         leftMostMovie.classList.add('hidden');
-        // Afficher le film suivant
         const nextMovie = categoryContainer.querySelector(`.video[data-movie-id="${categoryContainer.movies[startIndex + 3].id}"]`);
         nextMovie.classList.remove('hidden');
     }
@@ -82,16 +75,16 @@ function handleNextButton(categoryContainer, startIndex) {
             headers: {
                 'Origin': window.location.origin,
                 'Content-Type': 'application/json'
+                
             }
+            
         };
         fetch(url, requestOptions)
             .then(response => response.json())
             .then(data => {
                 if (data.hasOwnProperty("results")) {
                     const results = data["results"];
-                    // Stocker les films dans la propriété "movies" de chaque conteneur
                     container.movies = results;
-                    // Créer les éléments pour les films
                     for (const movie of results) {
                         let movieElement = document.createElement('div');
                         movieElement.classList.add('video');
@@ -104,8 +97,10 @@ function handleNextButton(categoryContainer, startIndex) {
                             <p>Score Imdb: ${movie.imdb_score}</p>
                         `;
                         container.appendChild(movieElement);
+                        console.log("API Response for", url, data); 
+
                     }
-                    // Afficher les premiers films
+
                     displayMovies(container, results, startIndex, 4);
                 } else {
                     console.error('Les données renvoyées ne sont pas dans le format attendu.');
@@ -117,8 +112,8 @@ function handleNextButton(categoryContainer, startIndex) {
     }
 
     const topRatedMoviesUrl = 'http://localhost:5000/api/v1/titles/?imdb_score_min=9.4';
-    fetchAndDisplayMovies(topRatedMoviesUrl, topRatedMoviesContainer, 1, startIndexCategory1);
-
+    fetchAndDisplayMovies(topRatedMoviesUrl, topRatedMoviesContainer, 1, 0);    
+    console.log(fetchAndDisplayMovies)
     const category1Url = 'http://localhost:5000/api/v1/titles/?genre=Horror';
     fetchAndDisplayMovies(category1Url, category1Container, 7, startIndexCategory1);
     const category2Url = 'http://localhost:5000/api/v1/titles/?genre=Action';
